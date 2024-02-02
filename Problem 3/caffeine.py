8da4d7d566ca37fb693fcf157597ac2bdb056a29
Emmie Kennemer <eek2654@utulsa.edu>
2024-02-02 16:57:44 -0600

    passed problem 3

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -5,9 +5,9 @@ def caffeine():
     #after_12 = caffeine_mg/4
     #after_24 = caffeine_mg/16
 
-    print(f' After 6 hours:{caffeine_mg/2:.2f} mg')
-    print(f' After 12 hours: {caffeine_mg/4:.2f} mg')
-    print(f' After 24 hours: {caffeine_mg/16:.2f} mg')
+    print(f'After 6 hours: {caffeine_mg/2:.2f} mg')
+    print(f'After 12 hours: {caffeine_mg/4:.2f} mg')
+    print(f'After 24 hours: {caffeine_mg/16:.2f} mg')
     
 
     
