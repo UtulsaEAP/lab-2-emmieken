858ec9d9ff477d0d31cd649ec2592f795ca388a1
Emmie Kennemer <eek2654@utulsa.edu>
2024-02-02 16:19:58 -0600

    not sure why problem 3 is failing

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,7 +1,15 @@
 
 def caffeine():
     caffeine_mg = float(input())
-    ''' Type your code here. '''
+    #after_6 = caffeine_mg/2
+    #after_12 = caffeine_mg/4
+    #after_24 = caffeine_mg/16
+
+    print(f' After 6 hours:{caffeine_mg/2:.2f} mg')
+    print(f' After 12 hours: {caffeine_mg/4:.2f} mg')
+    print(f' After 24 hours: {caffeine_mg/16:.2f} mg')
+    
+
     
 if __name__ == "__main__":
     caffeine()
