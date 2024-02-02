18fe9f2d87208ede4ab83563e4c1c09c95a51298
Emmie Kennemer <eek2654@utulsa.edu>
2024-02-02 17:30:54 -0600

    passed all problems

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,8 @@
 def telephone():
     phone_number = int(input())
+    prefix = int(phone_number % 10000000)
+    
+    print("(" + str(phone_number//10000000) + ") " + str(prefix//10000) + "-" + str(phone_number % 10000))
     ''' Type your code here. '''
 if __name__ == "__main__":
     telephone()
